ca2317b935292e1fa987bd83c28a48999cd63fd4
sahilpatel18 <sahil.41801@gmail.com>
2020-10-14 12:09:14 -0400

    created helper function to create blog post

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import {Text, View, StyleSheet} from 'react-native'
+import React,{useContext} from 'react'
+import {Text, View, StyleSheet, FlatList, Button} from 'react-native'
+import BlogContext from '../context/BlogContext'
+
 
 const IndexScreen = () => {
+const {data, addBlogPost} = useContext(BlogContext)
+
+
     return (
         <View>
-            <Text>Index</Text>
+            <Text>Index Screen</Text>
+            <Button title='Add Post' onPress={addBlogPost} />
+            <FlatList
+            data={data}
+            keyExtractor={(blogPost) => blogPost.title}
+            renderItem={({item}) => {
+                return (
+                    <Text>{item.title}</Text>
+                )
+            }}
+            />
         </View>
     )
 }
