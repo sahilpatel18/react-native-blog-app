396a8a05e369b74960ed242fcc810d7e08d61e58
sahilpatel18 <sahil.41801@gmail.com>
2020-10-14 19:15:51 -0400

    created show screen to display each blog post in detail

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import IndexScreen from './src/screens/IndexScreen';
 import { Provider } from './src/context/BlogContext';
+import ShowScreen from './src/screens/ShowScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Index" component={IndexScreen} />
+        <Stack.Screen name="Show" component={ShowScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,18 @@
+import React,{useContext} from 'react'
+import { Text, View, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native'
+
+
+const ShowScreen = () => {
+
+    return (
+        <View>
+          <Text>show</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default ShowScreen
